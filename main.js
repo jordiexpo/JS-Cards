f758fe7854ba5a945522098ecd24ba9056a3a758
Jordi Exposito <exjoru220806@gmail.com>
2024-04-24 10:01:38 +0200

    Card JS

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+const cartas = [
+    { nombre: 'Joker', imagen: './img/Joker.png' },
+    { nombre: 'As de corazones', imagen: './img/Ace-Spades.jpg' },
+];
+
 const card = document.querySelector('#card');
+const frontImage = document.querySelector('#front');
+
+
+function seleccionarCartaRandom() {
+    const indiceRandom = Math.floor(Math.random() * cartas.length);
+    return cartas[indiceRandom];
+}
+
+
 card.addEventListener('click', () => {
+    const cartaSeleccionada = seleccionarCartaRandom();
+    front.src = cartaSeleccionada.imagen;
+    front.alt = cartaSeleccionada.nombre;
     card.classList.toggle('flip');
 });
